2b3bd584dc522fa4675470c956fd35c4191739a1
Piotr Kosiński <piotrkosinski@gmail.com>
2021-06-18 13:53:39 +0200

    feat(dsb-message-broker): building docke containers

--- apps/dsb-message-broker/src/app.module.ts
+++ apps/dsb-message-broker/src/app.module.ts
@@ -3,8 +3,17 @@ import { ConfigModule } from '@nestjs/config';
 
 import { ChannelModule } from './channel/channel.module';
 import { MessageModule } from './message/message.module';
+import * as Joi from '@hapi/joi';
 
 @Module({
-    imports: [MessageModule, ChannelModule, ConfigModule.forRoot({ cache: true, isGlobal: true })]
+    imports: [
+        MessageModule,
+        ChannelModule,
+        ConfigModule.forRoot({
+            cache: true,
+            isGlobal: true,
+            validationSchema: Joi.object({ NATS_JS_URL: Joi.string().required() })
+        })
+    ]
 })
 export class AppModule {}
