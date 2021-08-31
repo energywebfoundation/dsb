dc8ee9c6428b93d41da9f444411233fecffcaf46
yartman <alireza.zerone@gmail.com>
2021-08-31 09:40:37 +0300

    fixing build issue

--- libs/dsb-transport-core/src/transport.interface.ts
+++ libs/dsb-transport-core/src/transport.interface.ts
@@ -7,14 +7,13 @@ export interface ITransport {
     //TODO: consider moving clientId to generic type like NATSJetstreamOptions if clientId is NATSJS specific
     pull(fqcn: string, amount: number, clientId: string): Promise<Message[]>;
     publish(fqcn: string, topic: string, payload: string): Promise<string>;
-    createChannel(channel: Channel, saveToAB?: boolean): Promise<string>;
+    createChannel(channel: Channel): Promise<string>;
     updateChannel(channel: Channel): Promise<string>;
     removeChannel(fqcn: string): Promise<string>;
-    getChannel(fqcn: string): Channel;
-    hasChannel(name: string): Promise<boolean>;
+    hasChannel(fqcn: string): Promise<boolean>;
     isConnected(): Promise<boolean>;
-    createConsumer(name: string, clientId: string): Promise<any>;
-    removeConsumer(name: string, clientId: string): Promise<boolean>;
+    createConsumer(fqcn: string, clientId: string): Promise<any>;
+    removeConsumer(fqcn: string, clientId: string): Promise<boolean>;
     hasConsumer(streamChannelName: string, consumer: string): Promise<boolean>;
     subscribe(
         fqcn: string,
@@ -24,6 +23,4 @@ export interface ITransport {
         socketId?: string
     ): Promise<any>;
     unsubscribe(subcriptionsId: number): void;
-    channelsToPublish(did: string, roles: string[]): Channel[];
-    channelsToSubscribe(did: string, roles: string[]): Channel[];
 }
